a2e2d313eaec8acaa4ad7516ae7fdcb9f004037c
Diana Badas <diana.badas@gmail.com>
2019-07-30 11:22:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+class Driver {
+  constructor(name, firstDay) {
+    this.name = name;
+    this.firstDay = firstDay;
+  }
 
+  startDate() {};
+
+  yearsExperienceFromBeginningOf() {};
+};
+
+class Route {
+  
+};
